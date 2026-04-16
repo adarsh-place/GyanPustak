7c643bcaf8f53bfc89e6edb709c809b0289b18b2
Adarsh Chandra <adarshworkplace@gmail.com>
2026-04-17 01:13:36 +0530

    Backend link added

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -11,7 +11,8 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        // target: 'http://localhost:5000',
+        target: 'https://gyanpustak-backend.onrender.com',
         changeOrigin: true,
       },
     },
